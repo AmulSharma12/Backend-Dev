3a9d20c4f1a47a82209cd62ef243f5984ae70e49
Amul Op <amulsharmarr67@gmail.com>
2024-09-11 23:01:28 +0530

    single line & multi-line comment

--- Javascript/01.Basics/app.js
+++ Javascript/01.Basics/app.js
@@ -1,4 +1,12 @@
 console.log("log is used for printing");
 console.clear();
-console.table({name:"Amul Sharma", age:20});
+console.table({ name: "Amul Sharma", age: 20 });
 console.log("object printed");
+
+// Single line comment
+
+/*
+Multi 
+line 
+comment
+*/
