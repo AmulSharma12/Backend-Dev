23b0ab16a9933ab41c9b23e53a93a957a7f6b53c
Amul Op <amulsharmarr67@gmail.com>
2024-09-13 01:36:45 +0530

    Primitive type - boolean ,  falsy values

--- Javascript/01.Basics/app.js
+++ Javascript/01.Basics/app.js
@@ -1,7 +1,7 @@
-console.log("log is used for printing");
-console.clear();
-console.table({ name: "Amul Sharma", age: 20 });
-console.log("object printed");
+// console.log("log is used for printing");
+// console.clear();
+// console.table({ name: "Amul Sharma", age: 20 });
+// console.log("object printed");
 
 // Single line comment
 
@@ -17,20 +17,40 @@ let whatDoYouWannaBecomeInYourLife = "Programmer";
 const gender = "MALE";
 let instagramHandle = "https://instagram.com/amul_op____";
 
-console.log(name);
-console.log(whatDoYouWannaBecomeInYourLife);
-console.log(gender);
-console.log(instagramHandle);
-
+// console.log(name);
+// console.log(whatDoYouWannaBecomeInYourLife);
+// console.log(gender);
+// console.log(instagramHandle);
 
 //================================ Primtives Types
 //Number type
 let num = 10;
-console.log(num + num);
-console.log(num - num);
-console.log(num * num);
-console.log(num / num);
-console.log(num % num);
-console.log(num ** num);  //power of number
-console.log(num++);
-console.log(num--);
+// console.log(num + num);
+// console.log(num - num);
+// console.log(num * num);
+// console.log(num / num);
+// console.log(num % num);
+// console.log(num ** num);  //power of number
+// console.log(num++);
+// console.log(num--);
+
+//Boolean type
+// let isTrue = true;
+// console.log(isTrue);
+
+//NaN  type
+let number = 78;
+console.log(number + undefined);
+
+/*
+
+Falsy and truthy value
+1. False
+2. null
+3. undefined
+4. '', "", `` (empty quotes)
+5. 0 
+6. -0
+7. NaN
+
+*/
