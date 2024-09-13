c35809104e2c467fe1c60c650679369198c0cf15
Amul Op <amulsharmarr67@gmail.com>
2024-09-13 23:42:40 +0530

    JS - switch statements

--- Javascript/02. Control Flow/app.js
+++ Javascript/02. Control Flow/app.js
@@ -9,3 +9,24 @@ let b = 40;
 if(a > b)   console.log("a is greater than b");
 else if(a < b)  console.log("a is less than b");
 else console.log("a is equals b");
+
+
+
+//=================================== Conditionals - Switch statements
+let x = 1;
+let bulb;
+switch(x){
+    case 0: 
+        bulb = "off";
+        console.log(bulb);
+        break;
+    case 1:
+        bulb = "on";
+        console.log(bulb);
+        break;
+    default:
+        bulb = "no value found";
+        console.log(bulb);
+        break;
+}
+
