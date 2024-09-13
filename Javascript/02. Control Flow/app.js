88532e9da0b55edb9bd8a344e5f501c59e4a90df
Amul Op <amulsharmarr67@gmail.com>
2024-09-14 00:17:34 +0530

    JS - loops

--- Javascript/02. Control Flow/app.js
+++ Javascript/02. Control Flow/app.js
@@ -4,29 +4,48 @@
 // else {....}
 
 
-let a = 40;
-let b = 40;
-if(a > b)   console.log("a is greater than b");
-else if(a < b)  console.log("a is less than b");
-else console.log("a is equals b");
+// let a = 40;
+// let b = 40;
+// if(a > b)   console.log("a is greater than b");
+// else if(a < b)  console.log("a is less than b");
+// else console.log("a is equals b");
 
 
 
 //=================================== Conditionals - Switch statements
-let x = 1;
-let bulb;
-switch(x){
-    case 0: 
-        bulb = "off";
-        console.log(bulb);
-        break;
-    case 1:
-        bulb = "on";
-        console.log(bulb);
-        break;
-    default:
-        bulb = "no value found";
-        console.log(bulb);
-        break;
+// let x = 1;
+// let bulb;
+// switch(x){
+//     case 0: 
+//         bulb = "off";
+//         console.log(bulb);
+//         break;
+//     case 1:
+//         bulb = "on";
+//         console.log(bulb);
+//         break;
+//     default:
+//         bulb = "no value found";
+//         console.log(bulb);
+//         break;
+// }
+
+
+//=================================== JS - Loops
+//------------------------- for loop and nested loop
+for(let ind = 0; ind <= 5; ind++)
+{
+    console.log('--------- outer loop --------------');
+    //nested loop
+    for(let j = 0; j<=3; j++){
+        console.log(ind , j);
+    }
 }
 
+
+//while loop   -    while(condition) {....}
+let i = 1;
+while(i<=3){
+    console.log(i);
+    i++;
+}
