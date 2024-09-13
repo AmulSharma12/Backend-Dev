6c9d2f5ca1a3510a1c1f116f99bb3db5b5149c52
Amul Op <amulsharmarr67@gmail.com>
2024-09-13 23:27:42 +0530

    JS - conditional statements

--- Javascript/02. Control Flow/app.js
+++ Javascript/02. Control Flow/app.js
@@ -1,1 +1,11 @@
-console.log("JS - Control flow");
+//=================================== Conditionals - if else-if else
+// if(condition) {....}
+// else if(condition) {.....}
+// else {....}
+
+
+let a = 40;
+let b = 40;
+if(a > b)   console.log("a is greater than b");
+else if(a < b)  console.log("a is less than b");
+else console.log("a is equals b");
