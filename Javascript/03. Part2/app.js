02f536ea3758cd789922a43920f325fcd8b22c6e
Amul Op <amulsharmarr67@gmail.com>
2024-09-14 23:12:08 +0530

    JS - Arrow Function challenge

--- Javascript/03. Part2/app.js
+++ Javascript/03. Part2/app.js
@@ -19,15 +19,32 @@
 // the lazy dog ${firstName} ${secondName}`;
 // console.log(templateString);
 
-
 //=================================== JS - Arrow function
-const nonParameterisedArrowFunction =  () => console.log("arrow function - non parameterised");
-const parameterisedArrowFunction = (name) => console.log(`parameterised arrow function - my name is ${name}`);
-const newFunction = (name) => {
-    console.log("arrow function");
-    console.log(`my name is ${name}`);
-}
+// const nonParameterisedArrowFunction =  () => console.log("arrow function - non parameterised");
+// const parameterisedArrowFunction = (name) => console.log(`parameterised arrow function - my name is ${name}`);
+// const newFunction = (name) => {
+//     console.log("arrow function");
+//     console.log(`my name is ${name}`);
+// }
 
-nonParameterisedArrowFunction();
-parameterisedArrowFunction("Amul Sharma");
-newFunction("Ritik roshan");
+// nonParameterisedArrowFunction();
+// parameterisedArrowFunction("Amul Sharma");
+// newFunction("Ritik roshan");
+
+//=================================== JS - Arrow Function Challenge
+setTimeout(() => {
+  console.log("function1 invoked");
+  setTimeout(() => {
+    console.log("Function 2 invoked");
+    setTimeout(() => {
+      console.log("Function 3 invoked");
+      setTimeout(() => {
+        console.log("Function 4 invoked");
+        setTimeout(() => {
+          console.log("Function 5 invoked");
+          alert("completed");
+        }, 2000);
+      }, 2000);
+    }, 2000);
+  }, 2000);
+}, 2000);
