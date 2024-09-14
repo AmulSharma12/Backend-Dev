5b4ff340fcc8d7706558ac04992a2b25af519e4f
Amul Op <amulsharmarr67@gmail.com>
2024-09-14 14:05:10 +0530

    JS - Callback function

--- Javascript/02. Control Flow/app.js
+++ Javascript/02. Control Flow/app.js
@@ -136,12 +136,9 @@
 // let sum = add(3, 4);
 // console.log(sum);
 
-
 // function add(x, y) {
 //   return x + y;
 // }
-
-
 
 //=================================== JS - Functions (Challenge)
 // function myFunction(value1, value2){
@@ -152,35 +149,64 @@
 // console.log(result);
 
 //=================================== JS - Function Declaration & Function Expression
-// We can declare function in two ways  - function declaration & function Expression
-// we use function keyword to create a function followed by name 
-functionDeclaration();
+// // We can declare function in two ways  - function declaration & function Expression
+// // we use function keyword to create a function followed by name
+// functionDeclaration();
 
-function functionDeclaration(){
-    console.log("this ways is called as function declaration also can be invoke before/after where it is created");
+// function functionDeclaration(){
+//     console.log("this ways is called as function declaration also can be invoke before/after where it is created");
+// }
+
+// functionDeclaration();
+
+// //function expression - By creating function without name assigning it to the variable. You can do it by 2 ways
+// //Points need to be remembered
+// //1. You must need to assign the function expression to the variable
+// //2. You can't invoke before you must need to invoke after the function expression
+
+// //--------------- Function expression 1st way
+// // functionExpressionFirstWay("Amul Sharma"); //error - ReferenceError: Cannot access 'functionExpressionFirstWay' before initialization
+
+// let functionExpressionFirstWay = function (firstExpressionWay){
+//     console.log(firstExpressionWay);
+// }
+
+// functionExpressionFirstWay("Amul Sharma");
+
+// //--------------- Function expression 2nd way - creating function expression using arrow function
+// let functionExpressionSecondWay =  (value) => {
+//     console.log(value);
+// }
+
+// functionExpressionSecondWay("Phil Dunkerley");
+
+
+
+//=================================== JS - Callback function
+//Passing function as argument to other function that function passed as argument is called as callback function 
+
+//example 1 
+function showCallFunc(callBackFn){
+    const value = 10;
+    callBackFn(value);
 }
 
-functionDeclaration();
 
 
-//function expression - By creating function without name assigning it to the variable. You can do it by 2 ways
-//Points need to be remembered
-//1. You must need to assign the function expression to the variable
-//2. You can't invoke before you must need to invoke after the function expression
-
-//--------------- Function expression 1st way
-// functionExpressionFirstWay("Amul Sharma"); //error - ReferenceError: Cannot access 'functionExpressionFirstWay' before initialization
-
-let functionExpressionFirstWay = function (firstExpressionWay){
-    console.log(firstExpressionWay);
-}
-
-functionExpressionFirstWay("Amul Sharma");
-
-//--------------- Function expression 2nd way - creating function expression using arrow function
-let functionExpressionSecondWay =  (value) => {
+showCallFunc(function(value){
     console.log(value);
+})
+
+
+//example 2
+function greetings(name, cb){
+    console.log(`Hi my name is ${name}`);
+    cb()
 }
 
 
-functionExpressionSecondWay();
+function callBackFn(){
+    console.log(`this is callback function`);
+}
+
+greetings("Amul Sharma", callBackFn);
