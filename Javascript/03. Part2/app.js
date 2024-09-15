5f4c3f2ad9048bcc6cc191f09e20624876006d00
Amul Op <amulsharmarr67@gmail.com>
2024-09-15 14:49:04 +0530

    JS - ES6 - spread operator

--- Javascript/03. Part2/app.js
+++ Javascript/03. Part2/app.js
@@ -113,13 +113,92 @@
 //=================================== JS - ES6 - Default Function Parameter
 //Default function parameter is ES6 feature in which we can provide the default value in parameter
 //so that while invoking the function if we dont provide the value it will take the default value
-let library =  () => {
-    return {
-        sum: (a,b = 0) => a + b,
-        multiply: (a,b = 1) => a * b
-    };
-}
+// let library =  () => {
+//     return {
+//         sum: (a,b = 0) => a + b,
+//         multiply: (a,b = 1) => a * b
+//     };
+// }
 
-const maths = library();
-console.log(maths.multiply(2,4));
-console.log(maths.sum(5,32));
+// const maths = library();
+// console.log(maths.multiply(2,4));
+// console.log(maths.sum(5,32));
+
+//=================================== JS - ES6 - Spread operator
+// Spread & rest operator uses three period (...)
+//Spread - unpack elements  --> [] , {}
+// ---> Use cases in array
+
+//1. Clone/Copying - it will clone everything from the updated array
+const outSports = ["Cricket", "FootBall"];
+const copySports = [...outSports];
+
+console.log(copySports);
+
+// 2. Merge - spread operator use for merging array
+const outDoorSports = ["Cricket", "Football"];
+const inDoorSports = ["Chess", "TableTennis"];
+const sports = [...outDoorSports, ...inDoorSports, "Hockey"];
+console.log(outDoorSports);
+console.log(inDoorSports);
+console.log(sports);
+
+// 3. spread operators in string
+const name = "Amul Sharma";
+const spreadNameArray = [...name];
+const spreadNameObject = { ...name };
+console.log(spreadNameArray);
+console.log(spreadNameObject);
+
+// ---> Use cases in Object
+
+// 1. cloning object using spread operator
+const studentData = {
+  name: "Amul Sharma",
+  age: 23,
+  class: 12,
+  sex: "MALE",
+  isAdult: true,
+};
+
+const amulData = { ...studentData };
+studentData.score = 89;
+console.log(studentData);
+console.log(amulData);
+
+// 2.  merging object using spread operator
+const personalData = {
+  name: "Amul Sharma",
+  age: 23,
+  class: 12,
+  sex: "MALE",
+  isAdult: true,
+};
+
+const address = {
+  city: "Mumbai",
+  state: "Maharashtra",
+};
+
+const personalDetails = { ...personalData, ...address };
+address.postCode = "2343";
+console.log(personalData);
+console.log(address);
+console.log(personalDetails);
+
+// 3. using spread operator shallow copy if nested properties exist - in such case reference being copied
+const patientDetails = {
+  firstName: "Neeraj",
+  LastName: "khanna",
+  Sex: "Male",
+  implantList: ["HipPrimary", "HipRevision", "KneePrimary"],
+};
+
+
+patientDetails.eirCode = "XLJLKJF89";
+const demoPatientDetails = {...patientDetails};
+patientDetails.countryCode = "India";
+patientDetails.implantList.push("KneeRevision");
+
+console.log(patientDetails);
+console.log(demoPatientDetails);
